89268e770c230f493780d6e28b0f27df51437cd5
tanjusay <112522489+tanjusay@users.noreply.github.com>
2023-05-17 00:23:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,39 +8,18 @@ st.write("Welcome to the YouTube Downloader app! With this app, you can easily d
 
 video_url = st.text_input("Enter the YouTube video URL:")
 
-if video_url:
+if st.button("Download"):
 
     try:
 
-        st.subheader("Download Options")
+        command = f"youtube-dl -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best {video_url}"
 
-        st.write("Select the video format and quality:")
+        subprocess.call(command, shell=True)
 
-        download_options = subprocess.check_output(['youtube-dl', '-F', video_url]).decode().strip().split('\n')[1:]
-
-        download_formats = [opt.split() for opt in download_options]
-
-        for format_code, extension, resolution, _, _ in download_formats:
-
-            st.write(f"Format Code: {format_code} | Extension: {extension} | Resolution: {resolution}")
-
-        selected_format = st.selectbox("Select a format:", [f"{format_code} ({extension})" for format_code, extension, _, _, _ in download_formats])
-
-        if st.button("Download"):
-
-            format_code = selected_format.split()[0]
-
-            download_path = f"{st.text_input('Enter the filename:', value='video')}.{selected_format.split()[2]}"
-
-            subprocess.call(['youtube-dl', '-f', format_code, '-o', download_path, video_url])
-
-            st.success(f"Video downloaded successfully. Saved as '{download_path}'")
+        st.success("Video downloaded successfully!")
 
     except Exception as e:
 
-        st.error(f"Error: {str(e)}")
-
-
-
+        st.error(f"An error occurred: {str(e)}")
 
 
